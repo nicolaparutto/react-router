9da5cad69b4d65070a79d193f6b2a04ae296dd87
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-23 16:45:52 +0100

    add: homepage layout and style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
 function HomePage() {
    return (
-      <h1>Home</h1>
+      <section className="hero-section">
+         <div className="hero-text">
+            <h1>Home Del Blog</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laboriosam vero debitis laborum. Eligendi minus provident explicabo exercitationem, culpa magni officia. Voluptas eaque placeat, repudiandae quos aspernatur nulla voluptatum.</p>
+         </div>
+      </section>
    )
 }
 
