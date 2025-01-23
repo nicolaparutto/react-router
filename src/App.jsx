245dc6a20e997b5c1835d2feea9409bd30bb8934
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-23 15:25:40 +0100

    feat: setup route operation for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+//Router:
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+//Pagine:
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import PostsPage from "./pages/PostsPage"
+
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+          <Route path="/posts" Component={PostsPage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
