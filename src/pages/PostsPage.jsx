382c87cbf1799ee3d345b96341c9847005581897
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-23 17:18:09 +0100

    feat: add postpage layout and style & dynamic post insertion

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,6 +1,25 @@
+//Import dati dei post:
+import postsData from "../data/postsData"
+
 function PostsPage() {
    return (
-      <h1>I nostri post</h1>
+      <section className="container">
+         <div className="posts-container">
+            {postsData.map(post => (
+               <div className="post" key={post.id}>
+                  <div className="post-content">
+                     <div className="post-image">
+                        <img src={post.url} alt="" />
+                     </div>
+                     <div className="post-text">
+                        <h1>{post.titolo}</h1>
+                        <p>{post.testo}</p>
+                     </div>
+                  </div>
+               </div>
+            ))}
+         </div>
+      </section>
    )
 }
 
