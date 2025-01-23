3998bc5163cac49091b59656e7da1f58323050c1
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-23 17:32:36 +0100

    feat: add about page layout and style

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,19 @@
 function AboutPage() {
    return (
-      <h1>Chi siamo</h1>
+      <section className="container">
+         <div className="about-content">
+            <div className="about-banner">
+               <div className="banner-text">
+                  <h1>Chi siamo?</h1>
+                  <span>Benvenuti nel nostro angolo di mondo!</span>
+                  <p>Siamo un gruppo di appassionati di viaggi, uniti dal desiderio di condividere esperienze, idee e ispirazione. Questo blog nasce dalla nostra voglia di raccontare storie, esplorare nuovi orizzonti e costruire una comunità di persone curiose e appassionate come noi.</p>
+               </div>
+               <div className="banner-image">
+                  <img src="img/about-img.jpeg" alt="" />
+               </div>
+            </div>
+         </div>
+      </section>
    )
 }
 
