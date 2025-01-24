9971cc301e832a3c76a054adbb59f60076312c92
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-24 14:13:05 +0100

    feat(bonus): add creation of new post, and redirected to the post list

--- src/pages/AddPostPage.jsx
+++ src/pages/AddPostPage.jsx
@@ -1,6 +1,73 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function AddPostPage() {
+
+   const navigate = useNavigate()
+   const endpointApi = "http://localhost:3000";
+
+   const initialPostData = {
+      title: "",
+      content: "",
+      image: "",
+      tags: []
+   }
+
+   const [postData, setPostData] = useState(initialPostData);
+
+   const handlerNewPost = (e) => {
+      setPostData((prevPostData) => ({
+         ...prevPostData,
+         [e.target.name]: e.target.value
+      }))
+      console.log(postData)
+   }
+
+   const handlerSendPost = (e) => {
+      e.preventDefault()
+      //conversione della stringa dei tag, in un array:
+      const tagsConversion = postData.tags.split(",").map(tag => tag.trim());
+
+      const updatedPost = {
+         ...postData,
+         tags: tagsConversion
+      }
+      //Effetto la chiamata all'API con il metodo post, e gli passo il nuovo oggetto:
+      axios.post(`${endpointApi}/posts`, updatedPost)
+         .then(res => {
+            //In fine resetto il form:
+            setPostData(initialPostData)
+            //Reindirizzo alla pagina della lista dei post:
+            navigate("/posts")
+         })
+   }
+
    return (
-      <div>AddPostPage</div>
+      <section className="container">
+         <div className="form-container">
+            <h1>Aggiungi un post</h1>
+            <form action="">
+               <div>
+                  <h4>Titolo del post</h4>
+                  <input type="text" name="title" placeholder='Inserisci il titolo' value={postData.title} onChange={handlerNewPost} />
+               </div>
+               <div>
+                  <h4>Immagine del post</h4>
+                  <input type="text" name="image" placeholder="Inserisci il link dell'immagine" value={postData.image} onChange={handlerNewPost} />
+               </div>
+               <div>
+                  <h4>Testo del post</h4>
+                  <input type="text" name="content" placeholder='Inserisci il testo' value={postData.content} onChange={handlerNewPost} />
+               </div>
+               <div>
+                  <h4>Tags del post (separati da una virgola!!!)</h4>
+                  <input type="text" name="tags" placeholder='Inserisci i tags' valuee={postData.tags} onChange={handlerNewPost} />
+               </div>
+               <button className='btn' onClick={handlerSendPost}>Crea</button>
+            </form>
+         </div>
+      </section>
    )
 }
 
